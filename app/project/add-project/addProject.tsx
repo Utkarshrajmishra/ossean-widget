2e1177d36c99311209eff9a943cfa22dd670a684
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2025-12-18 10:46:41 +0530

    Fix: NextAuth Callbacks

---
+++ app/project/add-project/addProject.tsx
@@ -0,0 +1,9 @@
+const AddProject=()=>{
+    return(
+        <section className="h-screen w-screen bg-gradient-to-br from-neutral-900 to-neutral-950">
+
+        </section>
+    )
+}
+
+export default AddProject
--- auth.ts
+++ auth.ts
@@ -1,52 +1,59 @@
 import NextAuth from "next-auth"
-import {prisma} from "./lib/prisma"
+import { prisma } from "./lib/prisma"
 import GoogleProvider from "next-auth/providers/google"
 import { JWT } from "next-auth/jwt"
-export const authOptions={
-    providers:[
+export const authOptions = {
+    providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID!,
+            clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
 
     ],
-      secret: process.env.NEXTAUTH_SECRET,
-      callbacks:{
-        async signIn({user}:any){
-            const {email, name}=user
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async signIn({ user }: any): Promise<boolean> {
+            const { email, name } = user
+
             try {
-                if(email && name){
-                    const existingUser=await prisma.user.findUnique({where:{email}})
-                    if(!existingUser){
-                        await prisma.user.create({data:{email,name}}) 
-                        return true
-                    } 
-                    return false
+                if (!email || !name) return false
+
+                const existingUser = await prisma.user.findUnique({
+                    where: { email },
+                })
+
+                if (!existingUser) {
+                    await prisma.user.create({
+                        data: { email, name },
+                    })
                 }
-                
+
+                return true
             } catch (error) {
-                console.log('Error during sign-in')
+                console.error("Error during sign-in", error)
                 return false
             }
+        },
 
-        }
-      },
+        async jwt({ token, account, user }: { token: JWT, account: any, user: any }) {
+            if (account && user) {
+                const { name, email } = user
 
-      async jwt({token, account, user}:{token:JWT, account:any, user:any}){
-        if(account && user){
-            const {name, email}=user
-            const newUser=await prisma.user.findUnique({where:{email}})
-            if(newUser) 
-                token.id=newUser.userId
+                const newUser = await prisma.user.findUnique({ where: { email } })
+                if (newUser)
+                    token.id = newUser.userId
                 return token
-            
+
+            }
+            return token
+        },
+        async session({ session, token }: { session: any, token: any }) {
+            Object.assign(session, { id: token.id })
+            return session
         }
-        return token
-      },
-      async session({session, token}:{session:any, token:any}){
-        Object.assign(session, {id:token.id})
-        return session
-      }
+    },
+
+
 }
-const handler=NextAuth(authOptions)
-export {handler as GET, handler as POST}
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
