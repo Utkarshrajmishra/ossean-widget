3c7727b743b89b8863fa681ef061bda77f11349a
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2025-12-30 13:02:42 +0530

    Fix: Prisma

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import prisma from "./lib/prisma"
+// import prisma from "./lib/prisma"
 import GoogleProvider from "next-auth/providers/google"
 import { JWT } from "next-auth/jwt"
 export const authOptions = {
@@ -11,47 +11,47 @@ export const authOptions = {
 
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async signIn({ user }: any): Promise<boolean> {
-            const { email, name } = user
+    // callbacks: {
+    //     async signIn({ user }: any): Promise<boolean> {
+    //         const { email, name } = user
 
-            try {
-                if (!email || !name) return false
+    //         try {
+    //             if (!email || !name) return false
 
-                const existingUser = await prisma.user.findUnique({
-                    where: { email },
-                })
+    //             const existingUser = await prisma.user.findUnique({
+    //                 where: { email },
+    //             })
 
-                if (!existingUser) {
-                    await prisma.user.create({
-                        data: { email, name },
-                    })
-                }
+    //             if (!existingUser) {
+    //                 await prisma.user.create({
+    //                     data: { email, name },
+    //                 })
+    //             }
 
-                return true
-            } catch (error) {
-                console.error("Error during sign-in", error)
-                return false
-            }
-        },
+    //             return true
+    //         } catch (error) {
+    //             console.error("Error during sign-in", error)
+    //             return false
+    //         }
+    //     },
 
-        async jwt({ token, account, user }: { token: JWT, account: any, user: any }) {
-            if (account && user) {
-                const { name, email } = user
+    //     async jwt({ token, account, user }: { token: JWT, account: any, user: any }) {
+    //         if (account && user) {
+    //             const { name, email } = user
 
-                const newUser = await prisma.user.findUnique({ where: { email } })
-                if (newUser)
-                    token.id = newUser.id
-                return token
+    //             const newUser = await prisma.user.findUnique({ where: { email } })
+    //             if (newUser)
+    //                 token.id = newUser.id
+    //             return token
 
-            }
-            return token
-        },
-        async session({ session, token }: { session: any, token: any }) {
-            Object.assign(session, { id: token.id })
-            return session
-        }
-    },
+    //         }
+    //         return token
+    //     },
+    //     async session({ session, token }: { session: any, token: any }) {
+    //         Object.assign(session, { id: token.id })
+    //         return session
+    //     }
+    // },
 
 
 }
