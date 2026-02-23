2940c3a59bd0c8d25489979d62779025ba37bdc5
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-02-23 11:36:02 +0530

    Enhance: FeedbackWidget layout and functionality

--- src/components/ui/widget.tsx
+++ src/components/ui/widget.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "./button";
 import { Textarea } from "./textarea";
+import { Label } from "./label";
 import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 import { Input } from "./input";
 import tailwindStyles from "../../index.css?inline";
@@ -42,22 +43,24 @@ const FeedbackWidget = () => {
           </PopoverTrigger>
 
           <PopoverContent
-            className="w-fit widget p-2 widget rounded-xl border border-gray-200 shadow-xl h-fit"
+            className="w-fit widget   widget rounded-xl border border-gray-200 shadow-md h-fit"
             align="end"
             sideOffset={8}
           >
             <style>{tailwindStyles}</style>
             {menu === 0 && (
-              <section className="flex flex-col gap-2 justify-center">
+              <section className="flex flex-col gap-6  justify-center">
                 {Menu?.map((item) => (
                   <div
                     onClick={() => setMenu(item.id)}
-                    className="flex items-start gap-3 p-2 rounded-lg hover:bg-neutral-100 transition-colors cursor-pointer"
+                    className="flex items-start gap-3  rounded-lg  transition-colors cursor-pointer"
                   >
-                    <div className="mt-0.5  text-neutral-400">{item.icon}</div>
+                    <div className="mt-0.5  text-neutral-400 size-6">
+                      {item.icon}
+                    </div>
 
                     <div className="flex flex-col">
-                      <p className="text-[0.83rem] leading-tight text-neutral-600 font-semibold">
+                      <p className="text-[0.83rem] leading-tight text-neutral-700 font-semibold">
                         {item.title}
                       </p>
                       <p className="text-xs text-neutral-400 leading-snug">
@@ -68,15 +71,134 @@ const FeedbackWidget = () => {
                 ))}
               </section>
             )}
-            {menu === 1 && (
-              <section>
-                <p className="flex gap-3 text-neutral-800 font-semibold font-sm"> <span className="text-neutral-400">{Menu[0].icon}</span> {Menu[0].title}</p>
-              </section>
+            {menu !== 0 && (
+              <>
+                <div className="">
+                  <form className="space-y-4">
+                    <div className="flex justify-between items-center">
+                      <p className="text-neutral-700   flex items-center gap-2 font-medium">
+                        <div className="size-5 text-sm text-neutral-400">
+                          {Menu[menu - 1].icon}
+                        </div>
+                        {Menu[menu - 1].title}
+                      </p>
+                      <div onClick={() => setMenu(0)}>
+                        {" "}
+                        <Cross className="size-5 cursor-pointer" size={4} />
+                      </div>
+                    </div>
+
+                    <div className="space-y-4">
+                      <Input
+                        id="name"
+                        required
+                        placeholder="Your name"
+                        className="h-9 bg-neutral-50 text-sm border-gray-300 focus:border-zinc-900 focus:ring-1 focus:ring-zinc-900 placeholder:text-gray-400 transition-colors"
+                      />
+                      <Input
+                        id="email"
+                        required
+                        placeholder="Your email"
+                        className="h-9 text-sm bg-neutral-50 border-gray-300 focus:border-zinc-900 focus:ring-1 focus:ring-zinc-900 placeholder:text-gray-400 transition-colors"
+                      />
+                      {menu !== 4 ? (
+                        <div>
+                          <Textarea
+                            id="feedback"
+                            required
+                            rows={4}
+                            placeholder={Menu[menu - 1].placeholder || ""}
+                            className="
+                    w-full min-h-[100px] 
+                    resize-none
+                    text-sm
+                    border-gray-300
+                    focus:border-zinc-900 
+                    bg-neutral-50
+                    focus:ring-1 
+                    focus:ring-zinc-900
+                    placeholder:text-gray-400
+                    transition-colors
+                  "
+                            maxLength={500}
+                          />
+                          <div className="text-xs text-gray-500 mt-1 text-right">
+                            Maximum 300 characters
+                          </div>
+                        </div>
+                      ) : (
+                        <div className="flex items-center gap-3">
+                          {[...new Array(5)].map((item, index) => (
+                            <Star className="size-5 cursor-pointer text-neutral-500" size={8} />
+                          ))}
+                         <p className="text-md font-semibold text-neutral-500"> Stars</p>
+                        </div>
+                      )}
+                    </div>
+                    {menu !== 4 && (
+                      <Button
+                        variant="outline"
+                        className="
+                h-9
+                rounded-2xl 
+                w-full 
+                text-sm
+              bg-neutral-50
+                font-medium
+                transition-colors
+              "
+                      >
+                        <Share size={8} /> Attach a image
+                      </Button>
+                    )}
+
+                    <Button
+                      type="submit"
+                      className="
+                h-9
+                text-sm
+                bg-zinc-900/85 
+                rounded-2xl 
+                w-full 
+               font-md
+                transition-colors
+                hover:bg-zinc-900
+              "
+                    >
+                      Send 
+                    </Button>
+                  </form>
+
+                  <p className="text-xs text-gray-700 mt-4 text-center">
+                    Built with  <span className="font-semibold underline">FeedMeBack</span>
+                  </p>
+                </div>
+              </>
             )}
           </PopoverContent>
         </Popover>
       </div>
     </>
+  );
+};
+
+const Cross = ({ size, className }: { size: number; className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={className}
+    >
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
+    </svg>
   );
 };
 
@@ -245,25 +367,32 @@ const Menu = [
   {
     title: "Give Feedback",
     desc: "Share your thoughts & insights.",
-    icon: <Mic size={20} className="size-6 " />,
+    icon: <Mic size={20} className=" " />,
+    placeholder: "Leave us your feedback!",
+    tag: "feedback",
     id: 1,
   },
   {
     title: "Reports a bug",
     desc: "Help us improve with your catches.",
-    icon: <ReportBug size={20} className="size-6 text-neutral-400" />,
+    icon: <ReportBug size={20} className=" text-neutral-400" />,
+    tag: "bug-report",
+    placeholder: "Where is the bug that you are experiencing?",
     id: 2,
   },
   {
     title: "Feature Request",
     desc: "Share your heart with us publicly.",
-    icon: <Notebook size={20} className="size-6 text-neutral-400" />,
+    tag: "feature-request",
+    icon: <Notebook size={20} className=" text-neutral-400" />,
+    placeholder: "What feature are you looking for?",
     id: 3,
   },
   {
     title: "Rate your experience",
+    tag: "rating",
     desc: "Rate your quality level if interaction.",
-    icon: <Star size={20} className="size-6 text-neutral-400" />,
+    icon: <Star size={20} className=" text-neutral-400" />,
     id: 4,
   },
 ];
