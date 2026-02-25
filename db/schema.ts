313e12385647608f17a5824d2cf2c3ccf39ded70
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-02-25 10:47:10 +0530

    Add: DB scripts

---
+++ db/schema.ts
@@ -0,0 +1,24 @@
+import { sql } from "drizzle-orm";
+import { pgTable, text, uuid, timestamp } from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+  id: uuid("id")
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  organization_id: text("organization_id").notNull(),
+  name: text("name"),
+  email: text("email").notNull().unique(),
+  image: text("image"),
+  created_at: timestamp("created_at").default(sql`now()`),
+});
+
+
+export const metadata=pgTable("metadata",{
+  id: text("id")
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  user_email: text("user_email").notNull(),
+  business_name:text("business_name").notNull(),
+  website_url:text("website_url").notNull(),
+  external_link:text("external_link").notNull()
+})
