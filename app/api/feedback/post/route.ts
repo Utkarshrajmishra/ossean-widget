d913b8147e58823f593cbe7c5d04574d0e2ccfa3
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-02-26 13:56:04 +0530

    Feat: Feedback post

---
+++ app/api/feedback/post/route.ts
@@ -0,0 +1,59 @@
+import { db } from "@/db/client";
+import crypto from "crypto";
+import {feedback as Feedback} from "@/db/schema";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+export async function OPTIONS() {
+  return new Response(null, { headers: corsHeaders });
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const {
+      project_key,
+      name,
+      email,
+      tag,
+      organization_id,
+      message,
+      rating,
+      image_url,
+    } = body;
+
+    if (!project_key && !message && !name && !email && !organization_id) {
+      return new Response(
+        JSON.stringify({ error: "Missing required fields" }),
+        { status: 400, headers: corsHeaders },
+      );
+    }
+
+    await db.insert(Feedback).values({
+        organization_id: organization_id,
+        project_id: project_key,
+        tag:tag,
+        email:email,
+        name:name,
+        message:message,
+        rating:rating,
+        image_Url:''
+    })
+
+    return new Response(
+        JSON.stringify("Feedback created"),
+        {status:201, headers: corsHeaders }
+    )
+    
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: error }),
+      { status: 500, headers: corsHeaders }
+    );
+ 
+  }
+}
