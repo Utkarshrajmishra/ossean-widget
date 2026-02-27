2a7e383a31aaadeaadb6d25660ef4559489656d5
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-02-27 11:35:31 +0530

    Add: Use Store

---
+++ store/user-store.tsx
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface User {
+  name: string;
+  email: string;
+  id: string;
+  image: string;
+}
+
+type UserState = User & {
+  setUser: (user: User) => void;
+};
+
+const useUser = create<UserState>()(
+  persist(
+    (set) => ({
+      name: "",
+      email: "",
+      id: "",
+      image: "",
+      setUser: (user: User) => set(user),
+    }),
+    {
+      name: "user-storage",
+    },
+  ),
+);
