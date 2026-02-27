b082753b4cb3ea37af4b18f83905592ef8af44a3
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-02-27 11:26:10 +0530

    Add: Project Store with Zustand for project_id management

--- store/project-store.tsx
+++ store/project-store.tsx
@@ -2,27 +2,19 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 type OrganizationState = {
-  organization_id: string;
   project_id: string;
-  setOrganization: (id: string) => void;
   setProjectId: (id: string) => void;
-  removeAll: () => void;
 };
 
 const useOrganization = create<OrganizationState>()(
   persist(
     (set) => ({
-      organization_id: "",
       project_id: "",
 
-      setOrganization: (id: string) => set({ organization_id: id }),
-
       setProjectId: (id: string) => set({ project_id: id }),
-
-      removeAll: () => set({ organization_id: "", project_id: "" }),
     }),
     {
-      name: "organization-storage",
+      name: "project-storage",
     },
   ),
 );
